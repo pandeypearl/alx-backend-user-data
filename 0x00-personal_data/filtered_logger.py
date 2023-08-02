0d66b7e07ec2c3b5902af42cae1c39745ad81179
Pretty Pandey <prettypandeypearl@gmail.com>
2023-08-02 02:42:16 -0700

    Task 1. Log formatter

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -21,3 +21,27 @@ def filter_datum(
         pattern = r'({0}=)[^{1}]*({1})'.format(key, separator)
         message = re.sub(pattern, r'\1{}\2'.format(redaction), message)
     return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        """
+        Instantiation method.
+        Sets fileds for each instance.
+        """
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """
+        Formatting the LogRecord instance.
+        """
+        log = super(RedactingFormatter, self).format(record=record)
+        return filter_datum(self.fields, self.REDACTION, log, self.SEPARATOR)
