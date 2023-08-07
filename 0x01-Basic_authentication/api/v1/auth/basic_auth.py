47fba31f9d603e9b4e7357c53330a1071d0609e5
Pretty Pandey <prettypandeypearl@gmail.com>
2023-08-07 17:39:59 +0000

    pycodestyle api/v1/auth/basic_auth.py

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -13,9 +13,11 @@ class BasicAuth(Auth):
     """
     Inherits from Auth Moodule
     """
-    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
         """
-        Returns ase64 part of the Authorization header for a Basic Authentication
+        Returns ase64 part of the Authorization header for
+        a Basic Authentication
         """
         if (
            not authorization_header or
@@ -24,20 +26,24 @@ class BasicAuth(Auth):
             return None
         return authorization_header[6:]
 
-    def decode_base64_authorization_header(self, base64_authorization_header: str) -> str:
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str) -> str:
         """
-        Returns the decoded value of a Base64 string base64_authorization_header
+        Returns the decoded value of a Base64
+        string base64_authorization_header
         """
         if (
            not base64_authorization_header or
            not isinstance(base64_authorization_header, str)):
             return None
         try:
-            return base64.b64decode(base64_authorization_header).decode('utf-8')
+            return base64.b64decode(base64_authorization_header).decode(
+                'utf-8')
         except Exception as e:
             return None
 
-    def extract_user_credentials(self, decoded_base64_authorization_header: str) -> (str, str):
+    def extract_user_credentials(
+            self, decoded_base64_authorization_header: str) -> (str, str):
         """
         Returns the user email and password from the Base64 decoded value.
         """
@@ -46,9 +52,10 @@ class BasicAuth(Auth):
            not isinstance(decoded_base64_authorization_header, str) or
            ':' not in decoded_base64_authorization_header):
             return (None, None)
-        return tuple(decoded_base64_authorization_header.split(':',1))
+        return tuple(decoded_base64_authorization_header.split(':', 1))
 
-    def user_object_from_credentials(self, user_email: str, user_pwd: str) -> TypeVar('User'):
+    def user_object_from_credentials(
+            self, user_email: str, user_pwd: str) -> TypeVar('User'):
         """
         Returns the User instance based on his email and password.
         """
