6dd9660e36d37a9186f97bb26340366ef7f0330b
Pretty Pandey <prettypandeypearl@gmail.com>
2023-08-07 13:54:37 +0000

    Task 9. Basic - User credentials

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -21,3 +21,27 @@ class BasicAuth(Auth):
            not authorization_header.startswith("Basic ")):
             return None
         return authorization_header[6:]
+
+    def decode_base64_authorization_header(self, base64_authorization_header: str) -> str:
+        """
+        Returns the decoded value of a Base64 string base64_authorization_header
+        """
+        if (
+           not base64_authorization_header or
+           not isinstance(base64_authorization_header, str)):
+            return None
+        try:
+            return base64.b64decode(base64_authorization_header).decode('utf-8')
+        except Exception as e:
+            return None
+
+    def extract_user_credentials(self, decoded_base64_authorization_header: str) -> (str, str):
+        """
+        Returns the user email and password from the Base64 decoded value.
+        """
+        if (
+           not decoded_base64_authorization_header or
+           not isinstance(decoded_base64_authorization_header, str) or
+           ':' not in decoded_base64_authorization_header):
+            return (None, None)
+        return tuple(decoded_base64_authorization_header.split(':',1))
