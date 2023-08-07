9d5c829a1e17ac532fd738c394641661483d2494
Pretty Pandey <prettypandeypearl@gmail.com>
2023-08-07 14:36:16 +0000

    updating pi/v1/views/index.py

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -14,6 +14,24 @@ def status() -> str:
     return jsonify({"status": "OK"})
 
 
+@app_views.route('/unauthorized', method=['GET'], strict_slashes=False)
+def unauthorized() -> str:
+    """
+    GET /api/v1/unauthorized
+    Return: - Error
+    """
+    return abort(401)
+
+
+@app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
+def forbidden() -> str:
+    """
+    GET /api/v1/forbidden
+    Return: - Error
+    """
+    return abort(403)
+
+
 @app_views.route('/stats/', strict_slashes=False)
 def stats() -> str:
     """ GET /api/v1/stats
@@ -24,12 +42,3 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
-
-
-@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
-def unauthorized() -> str:
-    """
-    GET /api/v1/unauthorized
-    Return: - Aborts
-    """
-    abort(403)
