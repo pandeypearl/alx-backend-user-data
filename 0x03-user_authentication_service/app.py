c737b2fe706f68cb62701ac91ee8047eb02288e0
Pretty Pandey <prettypandeypearl@gmail.com>
2023-08-15 14:47:26 +0000

    Task 17. Get reset password token

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -79,5 +79,20 @@ def profile():
         return jsonify({"email": user.email}), 200
 
 
+@app.route('/reset_password', methods=["POST"])
+def get_reset_password_token():
+    """
+    Responds with a reset_token for password
+    """
+    email = request.form.get('email')
+    try:
+        reset_token = AUTH.get_reset_password_token(email)
+        if not reset_token:
+            abort(403)
+        return jsonify({"email": email, "reset_token": reset_token}), 200
+    except ValueError:
+        abort(403)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
